e0a86e9bd3c12d8baaf26b5512ac830806fc9cd0
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-04-15 21:58:31 +0300

    debugging

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -59,11 +59,7 @@ const Navbar = () => {
         }}>
             <div className="flex items-center justify-between w-full h-full px-2 2xl:px-16">
                 <Link href="/">
-                    <Image
-                        src={Logo}
-                        alt="logo"
-                        width="100"
-                        height="50"/>
+                    <Image src={Logo} alt="logo" width="100" height="50"/>
                 </Link>
 
                 <div>
@@ -95,7 +91,109 @@ const Navbar = () => {
                 </div>
             </div>
 
-            
+            <div
+                className={nav
+                ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
+                : ""}>
+                {/* Side Drawer Menu */}
+                <div
+                    className={nav
+                    ? " fixed left-0 top-0 w-[90%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-8 ease" +
+                        "-in duration-500"
+                    : "fixed left-[-150%] top-0 p-10 ease-in duration-500"}>
+                    <div>
+                        <div className="flex w-full items-center justify-between">
+                            <Link href="/">
+                                <Image
+                                    src="/assets/29197369-removebg-preview.png"
+                                    width="87"
+                                    height="35"
+                                    alt="/"/>
+                            </Link>
+                            <div
+                                onClick={handleNav}
+                                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                                <AiOutlineClose/>
+                            </div>
+                        </div>
+                        <div className="border-b border-gray-300 my-4">
+                            <p className="w-[85%] md:w-[90%] py-4">
+                                Let&#39;s build something legendary together
+                            </p>
+                        </div>
+                    </div>
+                    <div className=" flex flex-col">
+                        <ul className="uppercase">
+                            <Link href="/">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                                    Home
+                                </li>
+                            </Link>
+                            <Link href="/#about">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                                    About
+                                </li>
+                            </Link>
+                            <Link href="/#skills">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                                    Skills
+                                </li>
+                            </Link>
+                            <Link href="/#projects">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                                    Projects
+                                </li>
+                            </Link>
+                            <Link href="/resume">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                                    Resume
+                                </li>
+                            </Link>
+                            <Link href="/#contact">
+                                <li onClick={() => setNav(false)} className="py-4 text-sm">
+                                    Contact
+                                </li>
+                            </Link>
+                        </ul>
+                        <div className="pb-4 pt-2">
+                            <p className="uppercase tracking-widest text-[#5651e5]">
+                                Let&#39;s Connect
+                            </p>
+                            <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                                <Link
+                                    href="https://www.linkedin.com/in/clint-briley-50056920a/"
+                                    target="_blank"
+                                    rel="noreferrer">
+                                    <div
+                                        className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                        <FaLinkedinIn/>
+                                    </div>
+                                </Link>
+                                <Link href="#" target="_blank" rel="noreferrer">
+                                    <div
+                                        className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                        <FaGithub/>
+                                    </div>
+                                </Link>
+                                <Link href="/#contact">
+                                    <div
+                                        onClick={() => setNav(!nav)}
+                                        className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                        <AiOutlineMail/>
+                                    </div>
+                                </Link>
+                                <Link href="/resume">
+                                    <div
+                                        onClick={() => setNav(!nav)}
+                                        className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                                        <BsFillPersonLinesFill/>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             {/* <div
                 className={nav

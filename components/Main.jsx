7396faebc9439aa0c2debb72af679587860c2fb1
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-04-20 17:39:45 +0300

    tiny edits

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {FaLinkedinIn, FaGithub} from "react-icons/fa";
 import {AiOutlineMail} from "react-icons/ai";
 import {BsFillPersonLinesFill} from "react-icons/bs";
+import Link from 'next/link';
 
 const Main = () => {
     return (
@@ -13,8 +14,7 @@ const Main = () => {
                         LET{"'"}S BUILD SOMETHING TOGETHER
                     </p>
                     <h1 className="py-4 text-gray-700">
-                        Hi, I{"'"}m <span className="text-[#5651e5]">
-                            Mohammad</span>
+                        Hi, I{"'"}m<span className="text-[#5651e5]">Mohammad</span>
                     </h1>
                     <h1 className="pb-2 text-gray-700">A Front-End Web Developer</h1>
                     <p className="py-2 text-gray-600 sm:max-w-[70%] m-auto">
@@ -22,20 +22,24 @@ const Main = () => {
                             experieces. Currently, I{"'"}m focused on building responsive front-end web applications
                     </p>
                     <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
-                        <div
+                        <a
+                            href="https://www.linkedin.com/in/mohammad-hussein-b6a2911b4/"
+                            target="_blank"
+                            rel="noreferrer"
                             className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
                             <FaLinkedinIn/>
-                        </div>
-                        <div
+                        </a>
+                        <a
+                            href="https://github.com/mhmdhussein377"
+                            target="_blank"
+                            rel="noreferrer"
                             className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
-                            <a href="https://github.com/mhmdhussein377" target="_blank" rel="noreferrer">
-                                <FaGithub/>
-                            </a>
-                        </div>
-                        <div
+                            <FaGithub/>
+                        </a>
+                        <Link href="/#contact"
                             className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
                             <AiOutlineMail/>
-                        </div>
+                        </Link>
                         <div
                             className="rounded-full shadow-lg shadow-gray-400 p-5 cursor-pointer hover:scale-110 ease-in duration-300">
                             <BsFillPersonLinesFill/>
